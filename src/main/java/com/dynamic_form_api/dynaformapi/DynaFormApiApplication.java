b2df19ceccd9fccf67a4cb4e3f37a3e9a81f69af
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-09-18 16:43:52 -0300

    first commit

---
+++ src/main/java/com/dynamic_form_api/dynaformapi/DynaFormApiApplication.java
@@ -0,0 +1,13 @@
+package com.dynamic_form_api.dynaformapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynaFormApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynaFormApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dynamic_form_api/dynaformapi/DynaFormApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamic_form_api.dynaformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynaFormApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
