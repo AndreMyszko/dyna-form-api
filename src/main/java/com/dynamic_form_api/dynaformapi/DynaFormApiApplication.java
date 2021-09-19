290f1f1752fb4cc372952ff99aae64f67fc0bbbf
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-09-19 16:50:47 -0300

    update. add swagger, main @Bean commented

--- src/main/java/com/dynamic_form_api/dynaformapi/DynaFormApiApplication.java
+++ src/main/java/com/dynamic_form_api/dynaformapi/DynaFormApiApplication.java
@@ -2,6 +2,12 @@ package com.dynamic_form_api.dynaformapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.info.Info;
+// import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class DynaFormApiApplication {
@@ -9,5 +15,15 @@ public class DynaFormApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DynaFormApiApplication.class, args);
 	}
+
+	// @Bean //adding swagger to api documentation -> usage: .../swagger-ui.html
+	// public OpenAPI customOpenAPI(@Value("${application.description}") String description){
+	// 	return new OpenAPI().info(new Info()
+	// 		.title(description)
+	// 		.version("1.0")
+	// 		.termsOfService("http://swagger.io/terms")
+	// 		.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	// }
+
 
 }
