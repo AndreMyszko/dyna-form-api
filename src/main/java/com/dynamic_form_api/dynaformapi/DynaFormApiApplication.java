5fbc80ec09b8f2d0cee958f77e5681f0dd5b1225
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-09-25 02:31:07 -0300

    add. swagger

--- src/main/java/com/dynamic_form_api/dynaformapi/DynaFormApiApplication.java
+++ src/main/java/com/dynamic_form_api/dynaformapi/DynaFormApiApplication.java
@@ -2,12 +2,12 @@ package com.dynamic_form_api.dynaformapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.info.Info;
-// import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class DynaFormApiApplication {
@@ -16,14 +16,14 @@ public class DynaFormApiApplication {
 		SpringApplication.run(DynaFormApiApplication.class, args);
 	}
 
-	// @Bean //adding swagger to api documentation -> usage: .../swagger-ui.html
-	// public OpenAPI customOpenAPI(@Value("${application.description}") String description){
-	// 	return new OpenAPI().info(new Info()
-	// 		.title(description)
-	// 		.version("1.0")
-	// 		.termsOfService("http://swagger.io/terms")
-	// 		.license(new License().name("Apache 2.0").url("http://springdoc.org")));
-	// }
+	@Bean //adding swagger to api documentation -> usage: .../swagger-ui.html <--> create application.description on application.properties file*
+	public OpenAPI customOpenAPI(@Value("${application.description}") String description){
+		return new OpenAPI().info(new Info()
+			.title(description)
+			.version("1.0")
+			.termsOfService("http://swagger.io/terms")
+			.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
 
 
 }
